7cbd557e05b558e3db82b90e3c3c58a5186e673a
Olha <189846437+sibaraku@users.noreply.github.com>
2026-01-21 17:24:05 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require('express')
-const path = require('path')
-const mysql = require('mysql2')
-const bodyParser = require('body-parser')
-
 const app = express()
+const path = require('path')
+const hbs = require('express-handlebars')
+const mysql = require('mysql2')
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts',
+}))
+
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const con = mysql.createConnection({
+var con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'qwerty',
   database: 'joga_mysql'
 })
 
-con.connect(err => {
-  if (err) throw err
-  console.log('Connected to MySQL database!')
+con.connect((err) => {
+    if (err) throw err
+    console.log('Connected to MySQL database')
+})
+
+app.get('/', (req, res) => {
+    let query = 'SELECT * FROM article'
+    let articles = []   
+    con.query(query, (err, results) => {
+        if (err) throw err
+        articles = results
+        res.render('index', { articles: articles })
+    })
 })
 
 app.listen(3003, () => {
