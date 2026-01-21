61c30c4b9eae5319eb71bc74fe7c9af673f693ee
Olha <189846437+sibaraku@users.noreply.github.com>
2026-01-21 17:42:52 +0200

    Displayed an article by slug

--- index.js
+++ index.js
@@ -39,6 +39,17 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article WHERE slug = '${req.params.slug}'`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err
+        article = result
+        console.log(article)
+        res.render('article', { article: article })
+    })
+})
+
 app.listen(3003, () => {
     console.log('Server is running on http://localhost:3003')
 })
